c02de115a4eb58bf1413af2ed6d8017de0ee7c38
Denis Roldan <denis.roldan.estebanez@gmail.com>
2016-01-20 21:59:34 +0100

    Modificado el DataManager para tener el servicio de búsqueda de artistas y creado el Presenter (Pasos 4 y 5?)

--- app/src/main/java/com/aiculabs/melchord/data/DataManager.java
+++ app/src/main/java/com/aiculabs/melchord/data/DataManager.java
@@ -10,26 +10,42 @@ import rx.functions.Action0;
 import rx.functions.Func1;
 import com.aiculabs.melchord.data.local.DatabaseHelper;
 import com.aiculabs.melchord.data.local.PreferencesHelper;
+import com.aiculabs.melchord.data.model.ArtistSearch;
 import com.aiculabs.melchord.data.model.Ribot;
+import com.aiculabs.melchord.data.remote.ArtistService;
 import com.aiculabs.melchord.data.remote.RibotsService;
 import com.aiculabs.melchord.util.EventPosterHelper;
 
 @Singleton
 public class DataManager {
 
+    /*
+    *
+    * The DataManager is the brain of the architecture.
+    * It extensively uses RxJava operators to combine, filter and transform data retrieved from helper classes.
+    * The aim of the DataManager is to reduce the amount of work that Activities and Fragments
+    * have to do by providing data that is ready to display and won’t usually need any transformation.
+    *
+    * */
+
+
     private final RibotsService mRibotsService;
+    private final ArtistService mArtistSearchService;
     private final DatabaseHelper mDatabaseHelper;
     private final PreferencesHelper mPreferencesHelper;
     private final EventPosterHelper mEventPoster;
 
+
     @Inject
-    public DataManager(RibotsService ribotsService, PreferencesHelper preferencesHelper,
+    public DataManager(RibotsService ribotsService, ArtistService artistSearchService, PreferencesHelper preferencesHelper,
                        DatabaseHelper databaseHelper, EventPosterHelper eventPosterHelper) {
         mRibotsService = ribotsService;
+        mArtistSearchService = artistSearchService;
         mPreferencesHelper = preferencesHelper;
         mDatabaseHelper = databaseHelper;
         mEventPoster = eventPosterHelper;
     }
+
 
     public PreferencesHelper getPreferencesHelper() {
         return mPreferencesHelper;
@@ -49,6 +65,16 @@ public class DataManager {
         return mDatabaseHelper.getRibots().distinct();
     }
 
+    // TODO - Esto funcionará?? xDD
+    public Observable<List<ArtistSearch>> getSearchResults(final String query){
+        return mArtistSearchService.getArtistSearchResults(query)
+                .concatMap(new Func1<List<ArtistSearch>, Observable<? extends List<ArtistSearch>>>() {
+                    @Override
+                    public Observable<? extends List<ArtistSearch>> call(List<ArtistSearch> artistSearches) {
+                        return mArtistSearchService.getArtistSearchResults(query);
+                    }
+                });
+    }
 
     /// Helper method to post events from doOnCompleted.
     private Action0 postEventAction(final Object event) {
---
+++ app/src/main/java/com/aiculabs/melchord/ui/search/SearchPresenter.java
@@ -0,0 +1,67 @@
+package com.aiculabs.melchord.ui.search;
+
+import com.aiculabs.melchord.data.DataManager;
+import com.aiculabs.melchord.data.model.ArtistSearch;
+import com.aiculabs.melchord.ui.base.BasePresenter;
+import com.aiculabs.melchord.ui.main.MainMvpView;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
+
+public class SearchPresenter extends BasePresenter<SearchMvpView> {
+    private final DataManager mDataManager;
+    private Subscription mSubscription;
+
+    @Inject
+    public SearchPresenter(DataManager dataManager) {
+        mDataManager = dataManager;
+    }
+
+    @Override
+    public void attachView(SearchMvpView mvpView) {
+        super.attachView(mvpView);
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        if (mSubscription != null) mSubscription.unsubscribe();
+    }
+
+    public void search(String query) {
+        checkViewAttached();
+
+        mSubscription = mDataManager.getSearchResults(query)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<List<ArtistSearch>>() {
+                    @Override
+                    public void onCompleted() {
+                        //TODO - Se supone que auí tendremos más cosillas...
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "There was an error searching...");
+                        getMvpView().showError();
+                    }
+
+                    @Override
+                    public void onNext(List<ArtistSearch> artistSearches) {
+                        if (artistSearches.isEmpty()) {
+                            getMvpView().showNoResults();
+                        } else {
+                            getMvpView().showResults(artistSearches);
+                        }
+                    }
+                });
+    }
+
+}
