e3daef68282a6c384a76a8f9901c92d3136bdbba
Denis Roldan <denis.roldan.estebanez@gmail.com>
2016-01-20 23:52:07 +0100

    Terminado el paso 8, esto debería estar medianamente listo para funcionar xD

--- app/src/main/java/com/aiculabs/melchord/ui/search/SearchActivity.java
+++ app/src/main/java/com/aiculabs/melchord/ui/search/SearchActivity.java
@@ -6,18 +6,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.aiculabs.melchord.R;
 import com.aiculabs.melchord.data.model.ArtistSearch;
+import com.aiculabs.melchord.util.DialogFactory;
 
+import java.util.Collections;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+
 public class SearchActivity extends AppCompatActivity implements SearchMvpView {
+
+    @Inject
+    SearchPresenter mSearchPresenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+        mSearchPresenter.attachView(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,10 +39,19 @@ public class SearchActivity extends AppCompatActivity implements SearchMvpView {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mSearchPresenter.search("Muse");
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mSearchPresenter.detachView();
     }
 
     @Override
@@ -38,11 +61,14 @@ public class SearchActivity extends AppCompatActivity implements SearchMvpView {
 
     @Override
     public void showNoResults() {
-        // TODO - Si no hay resultados, mostrar un aviso en la misma pantalla
+        // En caso de tener una lista de "cosas" sería necesario aquí vaciar el adapter
+        // y decirle al adapter que se actualice
+        Toast.makeText(this, R.string.empty_artists_or_songs, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void showError() {
-        // TODO - Si hay error de conexión o similar, mostrar aviso ;)
+        DialogFactory.createGenericErrorDialog(this, getString(R.string.error_loading_artists))
+                .show();
     }
 }
